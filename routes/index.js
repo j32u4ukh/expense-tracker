e4e4b8cb4424bc5c53e6a556685d89ed947a28d8
j32u4ukh <j32u4ukh@gmail.com>
2024-02-17 12:07:45 +0800

    feat: 建立頁面與路由

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,12 @@ router.get("/", (req, res) => {
   res.render("index");
 });
 
+router.get("/new", (req, res) => {
+  res.render("new");
+});
+
+router.get("/:id", (req, res) => {
+  res.render("edit");
+});
+
 module.exports = router;
