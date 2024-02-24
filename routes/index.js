8b805f7df7935bce98db8e929602a08da6f89718
j32u4ukh <j32u4ukh@gmail.com>
2024-02-24 17:03:12 +0800

    feat: 新增登入與註冊頁面與路由

--- routes/index.js
+++ routes/index.js
@@ -9,12 +9,24 @@ const CATEGORY = {
   其他: "https://fontawesome.com/icons/pen?style=solid",
 };
 
-// 支出記錄路由
+// 支出記錄路由 API
 const apis = require("./apis/index");
 router.use("/apis", apis);
 
 // 支出記錄路由
 const records = require("./records");
 router.use("/records", records);
+
+router.get("/", (req, res) => {
+  return res.redirect("/login");
+});
+
+router.get("/register", (req, res) => {
+  res.render("register");
+});
+
+router.get("/login", (req, res) => {
+  res.render("login");
+});
 
 module.exports = router;
