c8099269418c7d00756647752a62a89cba41da3c
j32u4ukh <j32u4ukh@gmail.com>
2024-02-28 12:20:50 +0800

    feat: 添加錯誤處理中間件

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -1,5 +1,5 @@
 module.exports = (error, req, res, next) => {
-  console.error(error);
+  console.error(`Error: ${JSON.stringify(error)}`);
   req.flash("error", error.errorMessage || "處理失敗:(");
   if (error.redirect) {
     res.redirect(error.redirect);
--- routes/records.js
+++ routes/records.js
@@ -34,22 +34,34 @@ router.get("/new", (req, res) => {
   });
 });
 
-router.post("/new", (req, res) => {
+router.post("/new", (req, res, next) => {
   const BODY = req.body;
   if (!utils.isValidParam(BODY, "name")) {
-    console.log("name 為必填欄位");
+    next({
+      redirect: "/records",
+      errorMessage: "name 為必填欄位",
+    });
     return;
   }
   if (!utils.isValidParam(BODY, "date")) {
-    console.log("date 為必填欄位");
+    next({
+      redirect: "/records",
+      errorMessage: "date 為必填欄位",
+    });
     return;
   }
   if (!utils.isValidParam(BODY, "amount")) {
-    console.log("amount 為必填欄位");
+    next({
+      redirect: "/records",
+      errorMessage: "amount 為必填欄位",
+    });
     return;
   }
   if (!utils.isValidParam(BODY, "categoryId")) {
-    console.log("categoryId 為必填欄位");
+    next({
+      redirect: "/records",
+      errorMessage: "categoryId 為必填欄位",
+    });
     return;
   }
   const { name, date, amount, categoryId } = BODY;
@@ -60,28 +72,39 @@ router.post("/new", (req, res) => {
   Record.add({ name, date, amount, userId, categoryId })
     .then((record) => {
       console.log(`record: ${JSON.stringify(record)}`);
+      req.flash("success", "新增支出紀錄成功!");
+      return res.redirect("/records");
     })
     .catch((err) => {
       console.log(`新增支出紀錄時發生錯誤, err: ${err}`);
-    })
-    .finally(() => {
-      res.redirect("/records");
+      next({
+        redirect: "/records",
+        errorMessage: `新增支出紀錄時發生錯誤, data: ${JSON.stringify(BODY)}`,
+      });
+      return;
     });
 });
 
-router.get("/:id", (req, res) => {
+router.get("/:id", (req, res, next) => {
   const id = req.params.id;
   const userId = req.user.id;
   console.log(`id: ${id}, userId: ${userId}`);
   Record.isExists(id, userId)
     .then((isExists) => {
       if (!isExists) {
-        console.log(`沒有 id 為 ${id} 的支出紀錄`);
-        return res.redirect("/records");
+        next({
+          redirect: "/records",
+          errorMessage: `User ${userId} 沒有 id 為 ${id} 的支出紀錄`,
+        });
+        return;
       }
       Record.get(id).then((record) => {
         if (!record) {
-          return res.redirect("/records");
+          next({
+            redirect: "/records",
+            errorMessage: `讀取支出紀錄(${id})時發生錯誤`,
+          });
+          return;
         }
         const data = Record.formatData(record);
         return res.render("edit", {
@@ -97,29 +120,44 @@ router.get("/:id", (req, res) => {
       });
     })
     .catch((error) => {
-      console.log(`error: ${error}`);
-      return res.redirect("/records");
+      next({
+        redirect: "/records",
+        errorMessage: `讀取支出紀錄時發生錯誤, error: ${error}`,
+      });
+      return;
     });
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", (req, res, next) => {
   const BODY = req.body;
   console.log(`BODY: ${JSON.stringify(BODY)}, type: ${typeof BODY}`);
   console.log(`name: ${BODY.name}`);
   if (!utils.isValidParam(BODY, "name")) {
-    console.log("name 為必填欄位");
+    next({
+      redirect: "/records",
+      errorMessage: "name 為必填欄位",
+    });
     return;
   }
   if (!utils.isValidParam(BODY, "date")) {
-    console.log("date 為必填欄位");
+    next({
+      redirect: "/records",
+      errorMessage: "date 為必填欄位",
+    });
     return;
   }
   if (!utils.isValidParam(BODY, "amount")) {
-    console.log("amount 為必填欄位");
+    next({
+      redirect: "/records",
+      errorMessage: "amount 為必填欄位",
+    });
     return;
   }
   if (!utils.isValidParam(BODY, "categoryId")) {
-    console.log("categoryId 為必填欄位");
+    next({
+      redirect: "/records",
+      errorMessage: "categoryId 為必填欄位",
+    });
     return;
   }
   const { name, date, amount, categoryId } = BODY;
@@ -132,50 +170,67 @@ router.put("/:id", (req, res) => {
   Record.isExists(id, userId)
     .then((isExists) => {
       if (!isExists) {
-        console.log(`沒有 id 為 ${id} 的支出紀錄`);
-        return res.redirect("/records");
+        next({
+          redirect: "/records",
+          errorMessage: `User ${userId} 沒有 id 為 ${id} 的支出紀錄`,
+        });
+        return;
       }
       const record = { id, name, date, amount, userId, categoryId };
-      console.log(`New record: ${JSON.stringify(record)}`);
       Record.update(record)
         .then((ok) => {
           console.log(`ok: ${ok}`);
+          req.flash("success", "更新支出紀錄成功!");
+          return res.redirect("/records");
         })
         .catch((error) => {
-          console.log(`error: ${error}`);
-        })
-        .finally(() => {
-          return res.redirect("/records");
+          next({
+            redirect: "/records",
+            errorMessage: `更新支出紀錄時發生錯誤, error: ${error}`,
+          });
+          return;
         });
     })
     .catch((error) => {
-      console.log(`error: ${error}`);
+      next({
+        redirect: "/records",
+        errorMessage: `讀取支出紀錄時發生錯誤, error: ${error}`,
+      });
+      return;
     });
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", (req, res, next) => {
   const id = req.params.id;
   const userId = req.user.id;
   Record.isExists(id, userId)
     .then((isExists) => {
       if (!isExists) {
-        console.log(`沒有 id 為 ${id} 的支出紀錄`);
-        return res.redirect("/records");
+        next({
+          errorMessage: `User ${userId} 沒有 id 為 ${id} 的支出紀錄`,
+        });
+        return;
       }
       Record.delete(id)
         .then((ok) => {
           console.log(`delete result: ${ok}`);
+          req.flash("success", "刪除支出紀錄成功!");
+          return res.redirect("/records");
         })
         .catch((err) => {
           console.log(`delete err: ${err}`);
-        })
-        .finally(() => {
-          return res.redirect("/records");
+          next({
+            errorMessage: `刪除支出紀錄時發生錯誤, id: ${id}`,
+          });
+          return;
         });
     })
     .catch((error) => {
-      console.log(`delete error: ${error}`);
-      return res.redirect("/records");
+      next({
+        redirect: "/records",
+        errorMessage: `讀取支出紀錄時發生錯誤, error: ${error}`,
+      });
+      return;
     });
 });
 
